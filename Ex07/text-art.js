f0f038d52aa38669bc420d774519a359069f090c
Andrei <andywatta05@gmail.com>
2023-03-22 18:14:05 +0200

    added ex07

---
+++ Ex07/text-art.js
@@ -0,0 +1,26 @@
+var figlet = require('figlet');
+
+figlet('Andrei', function (err, data) {
+  if (err) {
+    console.log('Something went wrong...');
+    console.dir(err);
+    return;
+  }
+  console.log(data)
+});
+
+
+figlet.text('Niceee!', {
+  font: 'Ghost',
+  horizontalLayout: 'default',
+  verticalLayout: 'default',
+  width: 80,
+  whitespaceBreak: true
+}, function (err, data) {
+  if (err) {
+    console.log('Something went wrong...');
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
